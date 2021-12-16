fad0a5e419911c175ad739cc1886847a1038217f
Dron Rahangdale <81355585+Drontitan@users.noreply.github.com>
2021-12-16 19:02:40 +0530

    Update Example.js
    Removed the bugs

--- src/Example.js
+++ src/Example.js
@@ -4,13 +4,22 @@ import React, { useState } from "react";
 
 const Example = () => {
      const [value, setValue] = useState(0);
+     const [value2, setValue2] = useState(0);
     const reset = () => {
       setValue(0);
+      setValue2(0);
     };
     const complexIncrease = () => {
       setTimeout(() => {
-        setValue((prevState) => {
+        setValue2((prevState) => {
           return prevState + 1;
+        });
+      }, 2000);
+    };
+    const complexDecrease = () => {
+      setTimeout(() => {
+        setValue2((prevState) => {
+          return prevState - 1;
         });
       }, 2000);
     };
@@ -31,7 +40,13 @@ const Example = () => {
            </section>
            <section style={{ margin: "4rem 0" }}>
              <h2>more complex counter</h2>
-             <h1>{value}</h1>
+             <h1>{value2}</h1>
+             <button className="btn" onClick={complexDecrease}>
+               decrease  later
+             </button>
+             <button className="btn" onClick={reset}>
+               reset
+             </button>
              <button className="btn" onClick={complexIncrease}>
                increase later
              </button>
@@ -41,4 +56,3 @@ const Example = () => {
    }
 
 export default Example;
-
